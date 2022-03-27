b71342987c06d3c4164391fa8e9426e8aac066d8
Nikita Sergeev <Nikita.Sergeev@paragon.ru>
2022-03-27 21:02:56 +0300

    Update 1_1B

--- ITMO_1_1B.py
+++ ITMO_1_1B.py
@@ -1,5 +1,5 @@
 cost = [54,32,123,43,251,23,56,74,767,49,78]
-price = cost
-for i in range(len(cost)):
-    cost[i] += cost[i] * 0.2
+price = [54,32,123,43,251,23,56,74,767,49,78]
+for i in range(len(price)):
+    price[i] += price[i] * 0.2
 print(price)
