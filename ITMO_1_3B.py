5d1d248c3ae4f1fb7fdd83cc55a230ce1534f195
Nikita Sergeev <Nikita.Sergeev@paragon.ru>
2022-04-02 13:59:23 +0300

    Add 1_3B

--- ITMO_1_3B.py
+++ ITMO_1_3B.py
@@ -1,17 +1,31 @@
-from syslog import LOG_WARNING
-import names
+import names, re
 from random import randint
 
-list1 = [randint(1, 100) for i in range(10)]
-print(list1)
+#### First part ####
+
+list1 = [randint(1, 100) for i in range(100)]
 
 list2 = list1.copy()
 
 for i in range(len(list2)):
-    if i > 50:
-        i = "HIGH"
+    if list2[i] > 50:
+        list2[i] = "HIGH"
     else:
-        i = "LOW"
+        list2[i] = "LOW"
 
 print(list2)
 
+#### Second part ####
+
+namesAll = ([''.join(names.get_first_name()) for i in range(100)])
+
+namesAM = []
+namesOther = []
+pattern1 = re.compile("[^N-Z]")
+pattern2 = re.compile("[^A-M]")
+
+namesAM = list(filter(pattern1.match, namesAll))
+namesOther = list(filter(pattern2.match, namesAll))
+
+print(namesAM, "\nTotal names:", len(namesAM))
+print(namesOther, "\nTotal names:", len(namesOther))
